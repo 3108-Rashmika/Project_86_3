1e4106cbece926d6264b2648e37d11d21485c984
Rashmika0508 <2310080086@klh.edu.in>
2024-11-09 12:02:25 +0530

    3rd

--- Project_86_3/settings.py
+++ Project_86_3/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-932jc-j*$xuy$ix(e^2ytvg^l&mo(y=#!z=s0_h$2#q54cv=m&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
